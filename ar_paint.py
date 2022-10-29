28af43b458ae0662306972cb187c8102ab493502
cmrsantos <cmrsantos@ua.pt>
2022-10-29 12:31:20 +0100

    adiçao das imagens

--- ar_paint.py
+++ ar_paint.py
@@ -17,13 +17,37 @@ args = parser.parse_args()
 #use_shake_prevention = args.use_shake_prevention
 # use_video_stream = args.use_video_stream
 
+def selectbiggestComponents(image):
+    connectivity=8
+    nLabels, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity,cv2.CV_32S)
+    final_image = np.zeros(output.shape, dtype=np.uint8)
+    sizes = stats[1:, -1]
+    nLabels = nLabels - 1
+    x = None
+    y = None
+    final_image = np.zeros(output.shape, dtype=np.uint8)
+    largest_component=0
+
+    for k in range(0, nLabels):
+        if sizes[k] >= largest_component:
+            
+            x, y = centroids[k + 1]
+            largest_component = sizes[k]
+            final_image[output == k + 1] = 255
+
+    return (final_image, x, y)
+
 def normal_mode():
 
     print("Normal mode in execution...")
 
-    jsonfile = open('limits.json', 'r')
-    data = jsonfile.read()
-    print(data)
+    with args.json as file:
+        limits=json.load(file)
+        # print(limits)
+    limits_dict=limits['limits_dict']
+
+    lvlmax = np.array([limits_dict['B']['max'], limits_dict['G']['max'], limits_dict['R']['max']])
+    lvlmin = np.array([limits_dict['B']['min'], limits_dict['G']['min'], limits_dict['R']['min']])
 
     vid = cv2.VideoCapture(0)
     window_name = "Orignal"
@@ -56,8 +80,16 @@ def normal_mode():
         flip_video = cv2.flip(frame, 1)
         cv2.imshow(window_name, flip_video)
 
-        cv2.imshow(window_name2, flip_video)
-        cv2.imshow(window_name3, flip_video)
+        #display masked resulting frame
+        mask_frame = cv2.inRange(frame, lvlmin, lvlmax)
+        flip_video2 = cv2.flip(mask_frame, 1)
+        cv2.imshow(window_name2, flip_video2)
+        #mask largest component result frame
+        
+        mask_largest = selectbiggestComponents(mask_frame)
+        flip_video3 = cv2.flip(mask_largest[0], 1)
+        cv2.imshow(window_name3, flip_video3)
+        
         cv2.imshow(window_name4, blank_image)
 
         pressed_key = cv2.waitKey(1)
